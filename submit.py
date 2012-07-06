ec62283ff22731b951dac1fc82d364a264e86bd2
Matt Cowger <matt@cowger.us>
2012-07-06 10:42:14 -0700

    FTP changes
    Change FTP method to FTP
    Change where we MD5 the file and how we do it (to use md5sum)
    explicitly close the zip file better.

--- submit.py
+++ submit.py
@@ -1,30 +1,13 @@
 #!/usr/bin/python
 
-from ftplib import FTP
+
 import zipfile
 import os
 import string
 import random
 import glob
-import hashlib
 import subprocess
 import sys
-
-def md5_for_file(f, block_size=2**20):
-    md5 = hashlib.md5()
-    while True:
-        data = f.read(block_size)
-        if not data:
-            break
-        md5.update(data)
-    return md5.hexdigest()
-
-def upload(ftp, file):
-    ext = os.path.splitext(file)[1]
-    if ext in (".txt", ".htm", ".html"):
-        ftp.storlines("STOR " + file, open(file))
-    else:
-        ftp.storbinary("STOR " + file, open(file, "rb"), 1024)
 
 
 def findprocessbyname(processname):
@@ -35,9 +18,6 @@ def processrunning(processname):
 	if (len(findprocessbyname(processname)) > 0):
 		return True
 	return False	
-
-
-
 
 print "Checking for still running processes..."
 if (processrunning("resxtop")):
@@ -50,21 +30,22 @@ if (processrunning("resxtop")):
 
 randomstring = "".join(random.sample(string.letters+string.digits, 5))
 filebase="emcdata." + randomstring
-myfile = zipfile.ZipFile(filebase+".zip",'w')
+zipfilename = filebase + ".zip"
+myfile = zipfile.ZipFile(zipfilename,'w')
 for filename in glob.glob('*.csv'):
 	myfile.write(filename, os.path.basename(filename), zipfile.ZIP_DEFLATED)
 	os.remove(filename)
-myfile.close
+myfile.close()
 
 
-print "Created zip file for upload: " + filebase + ".zip"
+print "Created zip file for upload: " + zipfilename
+print "MD5 of the file is: " +  subprocess.Popen(["md5sum",zipfilename],stdout=subprocess.PIPE).stdout.read()
 print "Attempting to upload to EMC Secure FTP..."
-ftp = FTP('ftp.emc.com')
-ftp.login()
-ftp.cwd("/incoming")
-upload(ftp,filebase + ".zip")
-ftp.quit()
-print "File (possibly) uploaded.  Inform your EMC representative to check CSFTP for the following file: ftp://csftp.emc.com/incoming/" + filebase + ".zip"
+output = subprocess.Popen(["/usr/bin/curl","-T",zipfilename,"ftp://ftp.emc.com/incoming/"+zipfilename],stdout=subprocess.PIPE).stdout.read()
+print output
+
+
+print "File (possibly) uploaded.  Inform your EMC representative to check CSFTP for the following file: ftp://csftp.emc.com/incoming/" + zipfilename
 print "It should be ready in 20 minutes."
-print "MD5 of the file is: " + md5_for_file(open(filebase + ".zip"))
+
 print "...or you can simply email or FTP the above file manually."
